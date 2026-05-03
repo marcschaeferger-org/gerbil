3f95e2da255c4592a932b3297ed4e256a7bb7aa6
Marc Schäfer <git@marcschaeferger.de>
2026-05-04 00:21:10 +0200

    fix(otel): revert semconv version and correct deployment environment attribute
    Signed-off-by: Marc Schäfer <git@marcschaeferger.de>

--- internal/observability/otel/resource.go
+++ internal/observability/otel/resource.go
@@ -3,7 +3,7 @@ package otel
 import (
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/sdk/resource"
-	semconv "go.opentelemetry.io/otel/semconv/v1.40.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.18.0"
 )
 
 // newResource builds an OTel resource for the Gerbil service.
@@ -15,11 +15,11 @@ func newResource(serviceName, serviceVersion, deploymentEnv string) (*resource.R
 		attrs = append(attrs, semconv.ServiceVersion(serviceVersion))
 	}
 	if deploymentEnv != "" {
-		attrs = append(attrs, semconv.DeploymentEnvironmentName(deploymentEnv))
+		attrs = append(attrs, semconv.DeploymentEnvironment(deploymentEnv))
 	}
 
 	return resource.Merge(
 		resource.Default(),
-		resource.NewWithAttributes(semconv.SchemaURL, attrs...),
+		resource.NewSchemaless(attrs...),
 	)
 }
